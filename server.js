c1fd6de5187cd8f8443104cc4cc1ca9bba609d8c
Shubhangi Hingu <shubhangihingu@gmail.com>
2023-05-08 18:14:44 +0530

    hell

--- server.js
+++ server.js
@@ -2,45 +2,36 @@
 // where your node app starts
 
 // init project
-require("dotenv").config();
-var express = require("express");
+require('dotenv').config();
+var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-	res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-// Serve the frontend script needed in the "/" path
-app.get("/src/js/main.js", function (req, res) {
-	res.sendFile(__dirname + "/src/js/main.js");
+
+// your first API endpoint... 
+app.get("/api/whoami", function (req, res, next) {
+  res.json({
+    ipaddress: req.headers["x-forwarded-for"],
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"] 
+  });
 });
 
-app.get("/api/whoami", function (req, res) {
-	res.json({
-		ipaddress: req.ip,
-		language: req.get("Accept-Language"),
-		software: req.get("User-Agent"),
-	});
-});
 
-app.use((req, res, next) =>
-	res
-		.status(404)
-		.send(
-			'Sorry, unknown path. <a href="http://localhost:5000/api/whoami">Visit this instead page instead</a>'
-		)
-);
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-	console.log("Your app is listening on port " + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });
