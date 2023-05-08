676211cf4736ce3fdf9d4b072955b56b2b958c21
Shubhangi Hingu <shubhangihingu@gmail.com>
2023-05-08 17:37:12 +0530

    first code

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-// index.js
+// server.js
 // where your node app starts
 
 // init project
-require('dotenv').config();
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
+// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+
+const requestIp = require('request-ip');
+// inside middleware handler
+var ipMiddleware = function(req, res, next) {
+    const clientIp = requestIp.getClientIp(req); 
+    next();
+};
+// on localhost you'll see 127.0.0.1 if you're using IPv4 
+// or ::1, ::ffff:127.0.0.1 if you're using IPv6
+//As Connect Middleware
+app.use(requestIp.mw())
+
+app.get('/api/whoami', (req, res) => {
+var ipadress = req.clientIp;
+var language = req.acceptsLanguages();
+var software=req.get('User-Agent');
+    res.json({
+      ipadress: ipadress,
+      language:language[0],
+      software:software.slice(software.indexOf("(") + 1, software.indexOf(")"))
+    });
+});
+
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
